0e94681521ad5bfe2ff4cf7b226a5ca94f01857c
adikssl <sakanov2007@gmail.com>
2026-06-29 18:48:29 +0500

    chore: project init with express, typescript, security middleware

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+	res.status(200).json({ status: 'ok' });
+});
+
+
+
+export default app;
